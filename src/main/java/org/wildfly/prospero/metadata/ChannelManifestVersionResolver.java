57eb1c6f186afbdffb1b8901f143bf13bf8bd4a8
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2024-10-04 10:10:29 +0100

    Refactor ManifestVersionResolver to use new ChannelSession API

---
+++ src/main/java/org/wildfly/prospero/metadata/ChannelManifestVersionResolver.java
@@ -0,0 +1,17 @@
+package org.wildfly.prospero.metadata;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.jboss.galleon.util.HashUtils;
+import org.wildfly.channel.Channel;
+import org.wildfly.channel.ChannelManifest;
+import org.wildfly.channel.ChannelManifestCoordinate;
+import org.wildfly.channel.ChannelSession;
+import org.wildfly.channel.Repository;
+import org.wildfly.channel.RuntimeChannel;
+
+public class ChannelManifestVersionResolver {
+
+}
